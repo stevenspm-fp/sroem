a20848b6f1f64a4db20d3c6ceca317efea53b7fa
Kevin See <kevin.see@dfw.wa.gov>
2023-03-09 15:11:15 -0800

    updated function to provide ability to put a maximum value on net error predictions.

--- R/predict_neterr.R
+++ R/predict_neterr.R
@@ -7,6 +7,9 @@
 #' @param redd_df dataframe containing redd data including columns with covariates in selected observer error model
 #' @param species which species is being analyzed? This function chooses the appropriate error model for this species. Choices are \code{Steelhead} or \code{Chinook}, with \code{Steelhead} being the default.
 #' @param num_obs if species is \code{Steelhead}, which error model to use, for one or two observers? Default is \code{two}.
+#' @param err_ceiling should the predictions have a ceiling (maximum value) for the predicted net error? Default is \code{TRUE}.
+#' @param err_quant if \code{err_ceiling} is \code{TRUE}, and \code{err_threshold} is \code{NULL} (the default), use this quantile of the observed net error in the initial model dataset to set that maximum value of predicted net errors. Default value is \code{0.95} for the 95th quantile.
+#' @param err_threshold if \code{err_ceiling} is \code{TRUE}, use this to manually set a maximum value for predicted net errors.
 #'
 #' @import dplyr tidyr tibble
 #' @return tibble
@@ -14,7 +17,10 @@
 
 predict_neterr <- function(redd_df = NULL,
                            species = c("Steelhead", "Spring Chinook"),
-                           num_obs = c("two", "one")) {
+                           num_obs = c("two", "one"),
+                           err_ceiling = T,
+                           err_quant = 0.95,
+                           err_threshold = NULL) {
 
   if(is.null(redd_df)) {
     stop("Redd data not supplied.")
@@ -45,13 +51,15 @@ predict_neterr <- function(redd_df = NULL,
 
     if("exp_sp_total_log" %in% miss_covar & "exp_sp_total" %in% names(redd_df)) {
       redd_df <- redd_df |>
-        mutate(exp_sp_total_log = log(exp_sp_total))
+        dplyr::mutate(exp_sp_total_log = log(exp_sp_total))
+
       miss_covar <- attr(net_err_mod$terms, "term.labels")[!attr(net_err_mod$terms, "term.labels") %in% names(redd_df)]
     }
 
     if("naive_density_km" %in% miss_covar & sum(c("visible_redds", "reach_length_km") %in% names(redd_df)) == 2) {
       redd_df <- redd_df |>
-        mutate(naive_density_km = visible_redds / reach_length_km)
+        dplyr::mutate(naive_density_km = visible_redds / reach_length_km)
+
       miss_covar <- attr(net_err_mod$terms, "term.labels")[!attr(net_err_mod$terms, "term.labels") %in% names(redd_df)]
     }
 
@@ -61,38 +69,62 @@ predict_neterr <- function(redd_df = NULL,
   }
 
   pred_df <- redd_df %>%
-    mutate(data_id = 1:n()) %>%
-    tidyr::pivot_longer(any_of(covar_center$metric),
+    dplyr::mutate(data_id = 1:n()) %>%
+    tidyr::pivot_longer(dplyr::any_of(covar_center$metric),
                         names_to = "metric",
                         values_to = "value"
     ) %>%
-    left_join(covar_center,
+    dplyr::left_join(covar_center,
               by = "metric"
     ) %>%
-    mutate(value = (value - mean) / sd) %>%
-    select(-mean, -sd) %>%
+    dplyr::mutate(value = (value - mean) / sd) %>%
+    dplyr::select(-mean, -sd) %>%
     tidyr::pivot_wider(
       names_from = "metric",
       values_from = "value"
     ) %>%
-    bind_cols(predict(net_err_mod,
+    dplyr::bind_cols(predict(net_err_mod,
                       newdata = .,
                       backtransform = T,
                       type = "link",
                       se.fit = T
     ) %>%
       tibble::as_tibble() %>%
-      select(
+      dplyr::select(
         net_error = fit,
         net_error_se = se.fit
       ))
 
   pred_df <- pred_df %>%
-    select(-any_of(covar_center$metric)) %>%
-    left_join(redd_df %>%
-                mutate(data_id = 1:n())) %>%
-    select(-data_id) %>%
-    select(any_of(names(redd_df)), everything())
+    dplyr::select(-dplyr::any_of(covar_center$metric)) %>%
+    dplyr::left_join(redd_df %>%
+                       dplyr::mutate(data_id = 1:n())) %>%
+    dplyr::select(-data_id) %>%
+    dplyr::select(dplyr::any_of(names(redd_df)),
+                  dplyr::everything())
+
+
+
+  # check if any predictions are above threshold
+  if(err_ceiling) {
+    # set threshold
+    if(!is.null(err_threshold)) {
+      err_thres <- err_threshold
+    } else {
+      err_thres <- quantile(net_err_mod$y, err_quant)
+    }
+
+    # shrink any predicted values to the threshold, if they exceed it
+    pred_df <- pred_df |>
+      dplyr::mutate(
+        exceed_ceiling = dplyr::if_else(net_error > err_thres,
+                                        T, F),
+        dplyr::across(
+          net_error,
+          ~ dplyr::if_else(. > err_thres,
+                           err_thres,
+                           .)))
+  }
 
   return(pred_df)
 }
